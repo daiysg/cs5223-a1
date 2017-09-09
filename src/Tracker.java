558ccf816164957befe8dcb81c06eafb34a0a016
daiysg <daiysg91@gmail.com>
2017-09-09 20:07:24 +0800

    update tracker

--- src/Tracker.java
+++ src/Tracker.java
@@ -1,6 +1,35 @@
+import java.util.List;
+
 /**
  * Created by ydai on 9/9/17.
  */
 public class Tracker {
-    private GameStatus gameStatus;
+    private List<String> playerNameList;
+
+    private Integer n;
+    private Integer k;
+
+    public List<String> getPlayerNameList() {
+        return playerNameList;
+    }
+
+    public void setPlayerNameList(List<String> playerNameList) {
+        this.playerNameList = playerNameList;
+    }
+
+    public Integer getN() {
+        return n;
+    }
+
+    public void setN(Integer n) {
+        this.n = n;
+    }
+
+    public Integer getK() {
+        return k;
+    }
+
+    public void setK(Integer k) {
+        this.k = k;
+    }
 }
